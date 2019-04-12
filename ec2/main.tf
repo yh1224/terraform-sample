94edc5c1ce95ea36315af9c4190d32d1dc9f34f5
Yoshiharu Hiroser <yh1224@gmail.com>
2019-04-12 23:29:19 +0900

    add sample for EC2

---
+++ ec2/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+# Specify the provider and access details
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+# Security Group (EC2)
+resource "aws_security_group" "sg_ec2" {
+  name        = "${var.service}-sg-ec2"
+  description = "EC2"
+
+  tags {
+    Name      = "${var.service}-ec2"
+    Service   = "${var.service}"
+    Env       = "${var.env}"
+    CraetedBy = "${var.created_by}"
+  }
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# Key pair
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+# EC2
+resource "aws_instance" "ec2" {
+  ami                    = "${var.ec2_ami}"
+  instance_type          = "${var.ec2_instance_type}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.sg_ec2.id}"]
+
+  tags {
+    Name      = "${var.service}-ec2"
+    Service   = "${var.service}"
+    Env       = "${var.env}"
+    CreatedBy = "${var.created_by}"
+  }
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,41 @@
+variable "created_by" {
+  description = "Resource creator"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "ap-northeast-1"
+}
+
+variable "aws_profile" {
+  description = "AWS profile"
+  default     = "default"
+}
+
+variable "service" {
+  description = "Service name"
+  default     = "test"
+}
+
+variable "env" {
+  description = "Environment name"
+  default     = "development"
+}
+
+variable "key_name" {
+  description = "Key pair name"
+}
+
+variable "public_key_path" {
+  description = "Path to public key file"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type for EC2"
+  default     = "t2.micro"
+}
+
+variable "ec2_ami" {
+  description = "AMI for EC2"
+}
